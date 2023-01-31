98539e4118dc16c49faec8a7465f29a1768db97c
mosch <moschetta.adrien@live.fr>
2023-01-31 16:57:24 +0100

    Adding default theme + config

--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+/* Remove u*/
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: ["./src/**/*.{js,jsx,ts,tsx}",],
+    theme: {
+        extend: {},
+    },
+    daisyui: {
+      themes: [
+        {
+          default: {
+            "primary": "#661AE6",
+            "secondary": "#D926AA",
+            "accent": "#1FB2A5",
+            "neutral": "#191D24",
+            "base-100": "#2A303C",
+            "info": "#3ABFF8",
+            "success": "#36D399",
+            "warning": "#FBBD23",
+            "error": "#F87272",
+          },
+        },
+      ],
+    },
+    plugins: [require("daisyui")],
 }
